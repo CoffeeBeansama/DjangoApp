449861d5ebb50522960cc49cfda91560b8ef5c77
Francis Aigo <francisaigo@yahoo.com>
2023-12-19 21:07:15 +0800

    a

--- MainProject/MainApplication/urls.py
+++ MainProject/MainApplication/urls.py
@@ -5,12 +5,11 @@ from . import settings
 
 
 urlpatterns = [
-    path("", views.index,name="main"),
-    path("home/",views.home,name="home"),
-    path("home/add/",views.addMember,name="addmember"),
-    path("home/add/addrecord/",views.addRecord,name="addrecord"),
-    path("home/update/<int:id>",views.update,name="update"),
-    path("home/update/updaterecord/<int:id>",views.updateRecord,name="updaterecord"),
-    path("home/delete/<int:id>",views.delete,name="delete"),
-    path("home/delete/deleterecord/<int:id>",views.deleteRecord,name="deleterecord"),
+    path("",views.home,name="home"),
+    path("add/",views.addMember,name="addmember"),
+    path("add/addrecord/",views.addRecord,name="addrecord"),
+    path("update/<int:id>",views.update,name="update"),
+    path("update/updaterecord/<int:id>",views.updateRecord,name="updaterecord"),
+    path("delete/<int:id>",views.delete,name="delete"),
+    path("delete/deleterecord/<int:id>",views.deleteRecord,name="deleterecord"),
 ]
